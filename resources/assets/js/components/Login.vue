e4163e36ea3e131441c10a0d3ea484bbe9c0d195
DimaS <dimasmile@yahoo.com>
2019-03-02 18:36:47 +0200

    login form

--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="alert alert-danger" v-if="error">
+        <!-- <div class="alert alert-danger" v-if="error">
             <p>There was an error, unable to sign in with those credentials.</p>
         </div>
         <form autocomplete="off" @submit.prevent="login" method="post">
@@ -13,7 +13,44 @@
                 <input type="password" id="password" class="form-control" v-model="password" required>
             </div>
             <button type="submit" class="btn btn-default">Sign in</button>
-        </form>
+        </form> -->
+        <v-container luid fill-heightd>
+            <v-layout align-center justify-center>
+                <v-flex xs12 sm8 md6>
+                    <v-card class="elevation-12">
+                        <v-toolbar light color="grey lighten-4">
+                            <v-toolbar-title>Login</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-form method="POST" v-model="valid">
+                                <v-text-field
+                                    name="email"
+                                    v-model="email"
+                                    :rules="[rules.required, rules.email]"
+                                    label="E-mail"
+                                ></v-text-field>
+                                    <v-text-field
+                                    :append-icon="show ? 'visibility_off' : 'visibility'"
+                                    :rules="[rules.required, rules.min]"
+                                    :type="show ? 'text' : 'password'"
+                                    name="password"
+                                    label="Password"
+                                    hint="At least 6 characters"
+                                    v-model="password"
+                                    class="input-group--focused"
+                                    @click:append="show = !show"
+                                    >
+                                </v-text-field>
+                            </v-form>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn @click.prevent="login" dark color="grey darken-3">Login</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
@@ -22,8 +59,19 @@
     data(){
       return {
         email: null,
-        password: null,
-        error: false
+        password: '',
+        error: false,
+        valid: false,
+        show: false,
+        rules: {
+          required: value => !!value || 'Required.',
+          counter: value => value.length <= 20 || 'Max 20 characters',
+          min: v => v.length >= 6 || 'Min 6 characters',
+          email: value => {
+            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return pattern.test(value) || 'Invalid e-mail.'
+          }
+        }
       }
     },
     methods: {
