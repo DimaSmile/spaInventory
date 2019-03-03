931b26ef84a9b989e3e7e90607dc27755ff695cd
DimaS <dimasmile@yahoo.com>
2019-03-04 00:27:01 +0200

    created factories and seeds for set test data in tables

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,15 @@
+<?php
+// use App\Category;
+use Faker\Generator as Faker;
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    $categories = App\Category::pluck('id')->toArray();
+    return [
+        'name' => $faker->name,
+        'drop_price' => $faker->randomNumber(2),
+        'retail_price' => $faker->randomNumber(2),
+        'image_url' => 'http://via.placeholder.com/600x400',
+        'image_name' => $faker->name,
+        'category_id' => $faker->randomElement($categories)
+    ];
+});
---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+// use App;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Джинсы', 'кофты', 'костюмы', 'джинсовки_кожанки', 'брюки спортивки', 'футболки', 'куртки_зима'];
+
+        foreach ($categories as $categoryName) {
+            App\Category::create(['name' => $categoryName]);
+        }
+    }
+}
