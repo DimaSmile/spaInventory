eaca57390231adcf008ed264fcc9511c4e29d28f
DimaS <dimasmile@yahoo.com>
2019-03-03 12:12:19 +0200

    styled inventory dashboard

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -24,8 +24,7 @@
                         <v-text-field v-model="editedItem.name" label="Наименование"></v-text-field>
                     </v-flex>
                     <v-flex xs12 sm6 md4>
-                        <v-text-field 
-                            v-model="editedItem.image" @click='pickFile' prepend-icon='attach_file' label="Картинка"></v-text-field>
+                        <v-text-field v-model="editedItem.imageName" label="Выберети картинку" @click='pickFile' prepend-icon='attach_file'></v-text-field>
                         <input 
                             type="file"
                             style="display: none"
@@ -41,7 +40,16 @@
                         <v-text-field v-model="editedItem.retailPrice" label="Цена розница"></v-text-field>
                     </v-flex>
                     <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.sizes" label="Размеры"></v-text-field>
+                        <!-- <v-text-field v-model="editedItem.sizes" label="Размеры"></v-text-field> -->
+                        <v-select
+                            v-model="editedItem.sizes"
+                            :items="allSizes"
+                            :menu-props="{ maxHeight: '400' }"
+                            label="Размеры"
+                            multiple
+                            hint="Pick your favorite states"
+                            persistent-hint
+                        ></v-select>
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -57,15 +65,23 @@
     </v-toolbar>
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="products"
       class="elevation-1"
     >
       <template v-slot:items="props">
         <td>{{ props.item.name }}</td>
-        <td class="text-xs-right"><v-img height="186" width="126" :src="props.item.image"></v-img></td>
-        <td class="text-xs-right">{{ props.item.dropPrice }}</td>
-        <td class="text-xs-right">{{ props.item.retailPrice }}</td>
-        <td class="text-xs-right">{{ props.item.sizes }}</td>
+        <td class="text-xs-center"><v-img height="186" width="126" :src="props.item.imageUrl"></v-img></td>
+        <td class="text-xs-center">{{ props.item.dropPrice }}</td>
+        <td class="text-xs-center">{{ props.item.retailPrice }}</td>
+        <td class="text-xs-center">
+            <select size="8" multiple disabled>
+                <option  v-for="(size, key) in allSizes" :value="size" :key="key + props.item.name"
+                v-bind:style="[props.item.sizes.includes(size) ? {'background' :'green' } : {'background' :'red' }]"
+                >
+                {{ size }}
+                </option>
+            </select>
+        </td>
         <td class="justify-center layout px-0">
           <v-icon
             small
@@ -92,44 +108,46 @@
 <script>
   export default {
     data: () => ({
-        // imageName: '',
-        imageUrl: '',
-        imageFile: '',
         dialog: false,
+        selected: '',
+        multiple: "true",
+        allSizes: [29, 30 , 31, 32, 33, 34, 35, 36, 37],
         headers: [
-            {
-            text: 'Name',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-            },
-            { text: 'Image', value: 'image', sortable: false },
-            { text: 'Цена дроп', value: 'dropPrice' },
-            { text: 'Цена розница', value: 'retailPrice' },
-            { text: 'Размеры', value: 'sizes' },
+            { text: 'Name', align: 'left', sortable: false, value: 'name'},
+            { text: 'Image', align: 'center', value: 'imageUrl', sortable: false },
+            { text: 'Цена дроп', align: 'center', value: 'dropPrice' },
+            { text: 'Цена розница', align: 'center', value: 'retailPrice' },
+            { text: 'Размеры', align: 'center', value: 'sizes' },
             { text: 'Actions', value: 'action', sortable: false }
         ],
-        desserts: [],
+        products: [],
         editedIndex: -1,
         editedItem: {
             name: '',
-            image: 0,
+            imageName: '',
+            imageUrl: '',
+            imageFile: '',
             dropPrice: 0,
             retailPrice: 0,
-            sizes: 0
+            sizes: []
         },
         defaultItem: {
             name: '',
-            image: 0,
+            imageName: '',
+            imageUrl: '',
+            imageFile: '',
             dropPrice: 0,
             retailPrice: 0,
-            sizes: 0
+            sizes: []
         }
     }),
     computed: {
         formTitle () {
             return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-        }
+        },
+        // sortSizes (itemSizes) {
+            
+        // }
     },
     watch: {
         dialog (val) {
@@ -143,129 +161,129 @@
         pickFile () {
             this.$refs.image.click ()
         },
-		
-		onFilePicked (e) {
-			const files = e.target.files
-			if(files[0] !== undefined) {
-				this.editedItem.image = files[0].name
-				if(this.editedItem.image.lastIndexOf('.') <= 0) {
-					return
-				}
-				const fr = new FileReader ()
-				fr.readAsDataURL(files[0])
-				fr.addEventListener('load', () => {
-					this.imageUrl = fr.result
-					this.imageFile = files[0] // this is an image file that can be sent to server...
-				})
-			} else {
-				this.editedItem.image = ''
-				this.imageFile = ''
-				this.imageUrl = ''
-			}
-		},
+        
+        onFilePicked (e) {
+            const files = e.target.files
+            if(files[0] !== undefined) {
+                this.editedItem.imageName = files[0].name
+                if(this.editedItem.imageName.lastIndexOf('.') <= 0) {
+                    return
+                }
+                const fr = new FileReader ()
+                fr.readAsDataURL(files[0])
+                fr.addEventListener('load', () => {
+                    this.editedItem.imageUrl = fr.result
+                    this.editedItem.imageFile = files[0] // this is an image file that can be sent to server...
+                })
+            } else {
+                this.editedItem.imageName = ''
+                this.editedItem.imageFile = ''
+                this.editedItem.imageUrl = ''
+            }
+        },
         initialize () {
-            this.desserts = [
+            this.products = [
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 30 , 31, 32, 33, 34, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 30 , 31, 32, 33, 34, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 30 , 31, 32, 33, 34, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 30 , 31, 32, 33, 34, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 222,
-                sizes: 4.0
+                sizes: [29, 30 , 31, 32, 33, 34, 35]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [ 34, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [ 31, 32, 33, 34, 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 30 , 35, 36, 37]
             },
             {
                 name: 'Frozen Yogurt',
-                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
+                imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hX7ktSvWlatywBvUSLsUjMKGDvI1aAY9haPf4QOeOBFT1GRn',
                 dropPrice: 6.0,
                 retailPrice: 24,
-                sizes: 4.0
+                sizes: [29, 30 , 31, 32]
             }
             ]
         },
         editItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
+            this.editedIndex = this.products.indexOf(item)
             this.editedItem = Object.assign({}, item)
             this.dialog = true
         },
         deleteItem (item) {
-            const index = this.desserts.indexOf(item)
-            confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+            const index = this.products.indexOf(item)
+            confirm('Are you sure you want to delete this item?') && this.products.splice(index, 1)
         },
         close () {
             this.dialog = false
@@ -276,12 +294,32 @@
         },
         save () {
             if (this.editedIndex > -1) {
-            Object.assign(this.desserts[this.editedIndex], this.editedItem)
+                Object.assign(this.products[this.editedIndex], this.editedItem)
             } else {
-            this.desserts.push(this.editedItem)
+                this.products.push(this.editedItem)
             }
             this.close()
         }
     }
   }
 </script>
+
+<style scope>
+
+select {
+    padding: 3px;
+    width: 100%;
+    border-radius: 3px;
+    text-align: center;
+}
+
+select option{
+    color: white;
+}
+
+option{
+    border: 1px solid black;
+    margin: 1px;
+}
+
+</style>
