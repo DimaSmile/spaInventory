c4880ce64c70de9e79edb7f6708251ffff90adc2
DimaS <dimasmile@yahoo.com>
2019-03-12 09:29:04 +0200

    fix image url

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -21,7 +21,7 @@ class ProductResource extends Resource
                 'name' => $this->name,
                 'dropPrice' => $this->drop_price,
                 'retailPrice' => $this->retail_price,
-                'imageUrl' => $this->image_url,
+                'imageUrl' => 'storage/' . $this->image_url,
                 'sizes' => [29, 30 , 31, 32, 33, 34, 35, 36, 37],
                 // 'image_name' => $this->image_name,
                 // 'category_id' => $this->category_id,
